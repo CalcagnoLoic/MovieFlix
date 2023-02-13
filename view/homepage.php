ef051ff19fbc0c1d836fa21204fd6b6b1d526981
Loïc Calcagno <calcagnoloic93@gmail.com>
2023-02-13 18:01:22 +0100

    modifcation of homepage

--- view/homepage.php
+++ view/homepage.php
@@ -13,13 +13,11 @@ session_start();
     <title>MovieFlix</title>
     <link rel="stylesheet" href="frontend/assets/sass/homepage.css">
     <link rel="icon" href="frontend/assets/img/logo.png">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <header>
         <nav>
             <div class="block1">
-                
                 <h1>MovieFlix</h1>
                 <img src="frontend/assets/img/logo.png" alt="Logo responsive" class="img-responsive">
             </div>
@@ -32,110 +30,43 @@ session_start();
     </header>
     <main>
         <div class="site">
-        <h1>Bienvenue,  <?php 
-        if (!empty($_SESSION['Customer'])) {
-            echo $_SESSION['Customer'] . '!';
-        } else {
-            echo "veuillez vous connecter SVP!!";
-        }
-        ?>
-        </h1>
+            <?php
+            if (!empty($_SESSION['Customer'])) {
+                echo "
+                <div>
+                    <ul>
+                        <li>Liste des genres pour les films</li>
+                        <li>Recherche d'un film par son nom</li>
+                    </ul>
+                </div>";
+            }
+            ?>
 
-        <h1><?php 
-        if (empty($_SESSION['Customer'])) {
-            echo "Un abonnement est obligatoire pour pouvoir avoir accès à la suite du site...";
-        }
-        ?>
-        </h1>
-            <div class="categorie-action">
-                <h2>Action</h2>
-                <div id="slider-action"></div>
-                <div class="chevron">
-                    <img src="frontend/assets/img/chevron-gauche.png" alt="chevron gauche" id="g-action" class="btn">
-                    <img src="frontend/assets/img/chevron-droit.png" alt="chevron droit" id="d-action" class="btn">
-                </div>
-                
-            </div>
-            <div class="categorie-aventure">
-                <h2>Aventure</h2>
-                <div id="slider-aventure"></div>
-                <div class="chevron">
-                    <img src="frontend/assets/img/chevron-gauche.png" alt="chevron gauche" id="g-aventure" class="btn">
-                    <img src="frontend/assets/img/chevron-droit.png" alt="chevron droit" id="d-aventure" class="btn">
-                </div>
-            </div>
-            <div class="categorie-comedie">
-                <h2>Comédie</h2>
-                <div id="slider-comedie"></div>
-                <div class="chevron">
-                    <img src="frontend/assets/img/chevron-gauche.png" alt="chevron gauche" id="g-comedie" class="btn">
-                    <img src="frontend/assets/img/chevron-droit.png" alt="chevron droit" id="d-comedie" class="btn">
-                </div>
-            </div>
-            <div class="categorie-drame">
-                <h2>Drame</h2>
-                <div id="slider-drame"></div>
-                <div class="chevron">
-                    <img src="frontend/assets/img/chevron-gauche.png" alt="chevron gauche" id="g-drame" class="btn">
-                    <img src="frontend/assets/img/chevron-droit.png" alt="chevron droit" id="d-drame" class="btn">
-                </div>
-            </div>
-            <div class="categorie-fantastique">
-                <h2>Fantastique</h2>
-                <div id="slider-fantastique"></div>
-                <div class="chevron">
-                    <img src="frontend/assets/img/chevron-gauche.png" alt="chevron gauche" id="g-fantastique" class="btn">
-                    <img src="frontend/assets/img/chevron-droit.png" alt="chevron droit" id="d-fantastique" class="btn">
-                </div>
-            </div>
-            <div class="categorie-horreur">
-                <h2>Horreur</h2>
-                <div id="slider-horreur"></div>
-                <div class="chevron">
-                    <img src="frontend/assets/img/chevron-gauche.png" alt="chevron gauche" id="g-horreur" class="btn">
-                    <img src="frontend/assets/img/chevron-droit.png" alt="chevron droit" id="d-horreur" class="btn">
-                </div>
-            </div>
-            <div class="categorie-policier">
-                <h2>Policier</h2>
-                <div id="slider-policier"></div>
-                <div class="chevron">
-                    <img src="frontend/assets/img/chevron-gauche.png" alt="chevron gauche" id="g-policier" class="btn">
-                    <img src="frontend/assets/img/chevron-droit.png" alt="chevron droit" id="d-policier" class="btn">
-                </div>
-            </div>
-            <div class="categorie-docu">
-                <h2>Documentaire</h2>
-                <div id="slider-docu"></div>
-                <div class="chevron">
-                    <img src="frontend/assets/img/chevron-gauche.png" alt="chevron gauche" id="g-docu" class="btn">
-                    <img src="frontend/assets/img/chevron-droit.png" alt="chevron droit" id="d-docu" class="btn">
-                </div>
-            </div>
-            <div class="categorie-mystere">
-                <h2>Mystère</h2>
-                <div id="slider-mystere"></div>
-                <div class="chevron">
-                    <img src="frontend/assets/img/chevron-gauche.png" alt="chevron gauche" id="g-mystere" class="btn">
-                    <img src="frontend/assets/img/chevron-droit.png" alt="chevron droit" id="d-mystere" class="btn">
-                </div>
-            </div>
-            <div class="categorie-scifi">
-                <h2>Science Fiction</h2>
-                <div id="slider-scifi"></div>
-                <div class="chevron">
-                    <img src="frontend/assets/img/chevron-gauche.png" alt="chevron gauche" id="g-scifi" class="btn">
-                    <img src="frontend/assets/img/chevron-droit.png" alt="chevron droit" id="d-scifi" class="btn">
-                </div>
-            </div>
-            <div class="categorie-thriller">
-                <h2>Thriller</h2>
-                <div id="slider-thriller"></div>
-                <div class="chevron">
-                    <img src="frontend/assets/img/chevron-gauche.png" alt="chevron gauche" id="g-thriller" class="btn">
-                    <img src="frontend/assets/img/chevron-droit.png" alt="chevron droit" id="d-thriller" class="btn">
-                </div>
-            </div>
+            <h1>Bienvenue, <?php 
+            if (!empty($_SESSION['Customer'])) {
+                echo $_SESSION['Customer'] . '!';
+            } else {
+                echo "veuillez vous connecter SVP!!";
+            }
+            ?>
+            </h1>
+
+            <?php 
+            if (empty($_SESSION['Customer'])) {
+                echo "<h2>Un abonnement est obligatoire pour pouvoir avoir accès à la suite du site...</h2>";
+                echo "<img src='frontend/assets/img/oops.gif' class='connect'/>";
+            } else {
+                echo "
+                <div class='container'>
+                    <div id='slider'></div>
+                    <div class='chevron'>
+                        <img src='frontend/assets/img/chevron-gauche.png' alt='chevron gauche' id='g-chevron' class='btn'>
+                        <img src='frontend/assets/img/chevron-droit.png' alt='chevron droit' id='d-chevron' class='btn'>
+                    </div>
+                    </div>
+                </div>";
+            }
+            ?>
         </div>
     </main>
     <?php
@@ -146,18 +77,7 @@ session_start();
     $footer->view('template/footer');
 
     ?>
-    
     <script src="frontend/assets/js/apiKey.js"></script>
-    <script src="frontend/assets/js/carrouselAction.js"></script>
-    <script src="frontend/assets/js/carrouselAventure.js"></script>
-    <script src="frontend/assets/js/carrouselComedie.js"></script>
-    <script src="frontend/assets/js/carrouselDrame.js"></script>
-    <script src="frontend/assets/js/carrouselFantastique.js"></script>
-    <script src="frontend/assets/js/carrouselHorreur.js"></script>
-    <script src="frontend/assets/js/carrouselCrime.js"></script>
-    <script src="frontend/assets/js/carrouselDocumentaire.js"></script>
-    <script src="frontend/assets/js/carrouselMystere.js"></script>
-    <script src="frontend/assets/js/carrouselScifi.js"></script>
-    <script src="frontend/assets/js/carrouselThriller.js"></script>
+    <script src="frontend/assets/js/carrouselHomepage.js"></script>
 </body>
 </html>
